ff6b7506fea24f5f453de56a728391bddc06cd69
Surya K L <159749383+Surya-K-L@users.noreply.github.com>
2024-09-03 20:43:34 +0530

    word-list.js

---
+++ HangManGame/scripts/word-list.js
@@ -0,0 +1,54 @@
+const wordList=[
+    {
+        word: "color",
+        hint: "This property is responsible for changing the font color."
+    },
+    {
+        word:"backgroundcolor",
+        hint:"It combines two words and sets the color behind the content."
+    },
+    {
+        word:"textalign",
+        hint:"This property aligns the text horizontally."
+    },
+    {
+        word:"body",
+        hint:"It's a four-letter tag that contains most of the content of the webpage."
+    },
+    {
+        word:"padding",
+        hint:" This property deals with the space between the content and the border."
+    },
+    {
+        word:"margin",
+        hint:"This property affects the outer space of an element."
+    },
+    {
+        word:"fontfamily",
+        hint:"This property sets the typeface for the text."
+    },
+    {
+        word:"textshadow",
+        hint:"This property is a combination of text and another word related to light."
+    },
+    {
+        word:"position",
+        hint:"This property can be set to values like absolute, relative, fixed, etc."
+    },
+    {
+        word:"width",
+        hint:"This property controls how wide an element will be."
+    },
+    {
+        word:"function",
+        hint:"This keyword is essential for creating reusable blocks of code."
+    },
+    {
+        word:"pop",
+        hint:"This method is used to remove the last element from an array."
+    },
+    {
+        word:"push",
+        hint:"Which method adds one or more elements to the end of an array?"
+    }
+]
